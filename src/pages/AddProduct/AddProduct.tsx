140813b7400aeb4a73fed33618011cd6b2980ff4
LirakKutia <lirak@kutia.net>
2021-03-16 11:05:06 +0100

    Loading

--- src/pages/AddProduct/AddProduct.tsx
+++ src/pages/AddProduct/AddProduct.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from "react"
 import { useFormik } from 'formik';
-import * as API from '../../api/Api'
+import * as API from '../../api/Api';
+import { Button } from 'semantic-ui-react';
+
 
 export const Add = () => {
+  const [loading,setLoading] = useState(false);
   const formik = useFormik({
     initialValues: {
       regular_price: '',
       images:"",
       name: '',
     },
+
     onSubmit: async (values, formikHelpers) => {
       try {
         console.log(values)
@@ -16,10 +20,11 @@ export const Add = () => {
           ...values,
           images:[{src:values.images}]
         }
-         const res = await API.add(apiValues)
-        console.log("res", res)
+        setLoading(true);
+        const res = await API.add(apiValues)
         formikHelpers.resetForm()
       } catch (e) {}
+      setLoading(false);
     },
   });
   return (
@@ -31,6 +36,7 @@ export const Add = () => {
         type="text"
         onChange={formik.handleChange}
         value={formik.values.name}
+        disabled={loading}
       />
       <label htmlFor="regular_price">Price :</label>
       <input
@@ -38,6 +44,7 @@ export const Add = () => {
         name="regular_price"
         onChange={formik.handleChange}
         value={formik.values.regular_price}
+        disabled={loading}
       />
       <label htmlFor="images">Image Url :</label>
       <input
@@ -46,85 +53,14 @@ export const Add = () => {
         type="text"
         onChange={formik.handleChange}
         value={formik.values.images}
+        disabled={loading}
       />
-      <button type="submit">Submit</button>
+      {loading?
+        <Button loading>Loading</Button>
+        : <button type="submit">Submit</button>}
     </form>
   );
 };
 
 
-
-// import React, { useState } from "react"
-// import * as API from '../../api/Api'
-//
-//
-// export const Add = () => {
-//   const initialData = {
-//     name: "",
-//     price: "",
-//     img: "http://via.placeholder.com/250x250"
-//   };
-//
-//   const [submit, setSubmit] = useState();
-//   const [name, setName] =useState(initialData.name);
-//   const [price, setPrice] =useState(initialData.price);
-//   const [img, setImg] =useState(initialData.img);
-//
-//   const handleNameChange = (e:any) => {
-//     setName(e.target.value);
-//
-//   }
-//   const handlePriceChange = (e:any) =>{
-//     setPrice(e.target.value);
-//   }
-//   const handleImgChange = (e:any) =>{
-//     setImg(e.target.value);
-//     }
-//
-//     const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
-//       e.preventDefault();
-//       API.addProduct({})
-//     }
-//
-//   return (
-//     <form onSubmit={handleSubmit} className="ui form">
-//
-//       <div className="fields">
-//         <div className="five wide column">
-//           <img src={img} alt="image" className="ui image" />
-//         </div>
-//         <div className="field">
-//           <label>Product Name</label>
-//           <input
-//             type="text"
-//             placeholder="Product Name"
-//             onChange={handleNameChange}
-//           ></input>
-//         </div>
-//         <div className="field">
-//           <label>Price</label>
-//           <input
-//             type="number"
-//             placeholder="Price"
-//             onChange={handlePriceChange}
-//           ></input>
-//         </div>
-//         <div className="field">
-//           <label>Img url</label>
-//           <input
-//             type="text"
-//             placeholder="Image Url"
-//             value={initialData.img}
-//             onChange={handleImgChange}
-//           ></input>
-//           <button
-//             className="ui button primary"
-//             type="submit"
-//           >Submit
-//           </button>
-//         </div>
-//       </div>
-//     </form>
-//   );
-// }
 
