d72ba9b4eca73fba0860555d4d9c37e3a9408ecd
LirakKutia <lirak@kutia.net>
2021-03-17 14:21:49 +0100

    Add ProductCard

--- src/pages/ProductList/ProductCard/ProductCard.tsx
+++ src/pages/ProductList/ProductCard/ProductCard.tsx
@@ -3,38 +3,39 @@ import { Button, Card, Divider, Image, Placeholder } from "semantic-ui-react"
 import { Spinner } from "reactstrap"
 import { Link } from "react-router-dom"
 
-export const ProductForm = (props:any) => {
-  return( <div> </div>
-  //       <Card>
-  //         {loading ? (
-  //           <Placeholder>
-  //             <Placeholder.Image square />
-  //           </Placeholder>
-  //         ) : (
-  //           <Image className="card-img" src={product.images[0]?.src} />
-  //         )}
-  //
-  //         <Card.Content>
-  //           {loading ? (
-  //             <Spinner color="primary"/>
-  //           ) : (
-  //             <>
-  //               <Card.Header>{product.name}</Card.Header>
-  //               <Card.Description>Price: {product.price} $</Card.Description>
-  //             </>
-  //           )}
-  //         </Card.Content>
-  //
-  //         <Card.Content extra>
-  //           <Link to={`/products/show/${product.id}`}>
-  //             <Button disabled={loading} >Show</Button>
-  //           </Link>
-  //           <Link to={`/products/${product.id}`}>
-  //             <Button disabled={loading} primary>Edit</Button>
-  //           </Link>
-  //           {loading?
-  //             <Button loading>Loading</Button>:
-  //             <Button disabled={loading} onClick={()=>deleteProduct(product.id)}>Delete</Button>}
-  //         </Card.Content>
+export const ProductCard = (props:any) => {
+  return(
+    <Card>
+      {props.loading ? (
+        <Placeholder>
+          <Placeholder.Image square />
+        </Placeholder>
+      ) : (
+        <Image className="card-img" src={props.images} />
+      )}
+
+      <Card.Content>
+        {props.loading ? (
+          <Spinner color="primary"/>
+        ) : (
+          <>
+            <Card.Header>{props.name}</Card.Header>
+            <Card.Description>Price: {props.price} $</Card.Description>
+          </>
+        )}
+      </Card.Content>
+
+      <Card.Content extra>
+        <Link to={`/products/show/${props.id}`}>
+          <Button disabled={props.loading} >Show</Button>
+        </Link>
+        <Link to={`/products/${props.id}`}>
+          <Button disabled={props.loading} primary>Edit</Button>
+        </Link>
+        {props.deleteCheck===props.id?
+          <Button loading>Loading</Button>:
+          <Button disabled={props.loading} onClick={props.deleteProduct}>Delete</Button>}
+      </Card.Content>
+    </Card>
   );
 }
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -5,7 +5,7 @@ import { Button, Card, Divider, Image, Placeholder } from 'semantic-ui-react'
 import './ProductList.scss';
 import { EditProductForm } from '../EditProduct/EditProductForm';
 import {Spinner} from "reactstrap";
-
+import {ProductCard} from './ProductCard/ProductCard'
 
 export const ProductList = () => {
   const [data, setData] = useState([]);
@@ -60,38 +60,48 @@ return (
         : <div></div> }
       <Card.Group doubling itemsPerRow={3} stackable>
         {data?.map((product:any,index)=>(
-          <Card key={product.id}>
-            {loading ? (
-              <Placeholder>
-                <Placeholder.Image square />
-              </Placeholder>
-            ) : (
-              <Image className="card-img" src={product.images[0]?.src} />
-            )}
-
-            <Card.Content>
-              {loading ? (
-                <Spinner color="primary"/>
-              ) : (
-                <>
-                  <Card.Header>{product.name}</Card.Header>
-                  <Card.Description>Price: {product.price} $</Card.Description>
-                </>
-              )}
-            </Card.Content>
-
-            <Card.Content extra>
-              <Link to={`/products/show/${product.id}`}>
-                <Button disabled={loading} >Show</Button>
-              </Link>
-              <Link to={`/products/${product.id}`}>
-                <Button disabled={loading} primary>Edit</Button>
-              </Link>
-              {deleteCheck===product.id?
-                <Button loading>Loading</Button>:
-                <Button disabled={loading} onClick={()=>deleteProduct(product.id)}>Delete</Button>}
-            </Card.Content>
-          </Card>
+          <ProductCard
+          key={product.id}
+          id={product.id}
+          loading={loading}
+          name={product.name}
+          price={product.price}
+          deleteCheck={deleteCheck}
+          images={product.images[0]?.src}
+          deleteProduct={()=>deleteProduct(product.id)}
+          />
+          // <Card key={product.id}>
+          //   {loading ? (
+          //     <Placeholder>
+          //       <Placeholder.Image square />
+          //     </Placeholder>
+          //   ) : (
+          //     <Image className="card-img" src={product.images[0]?.src} />
+          //   )}
+          //
+          //   <Card.Content>
+          //     {loading ? (
+          //       <Spinner color="primary"/>
+          //     ) : (
+          //       <>
+          //         <Card.Header>{product.name}</Card.Header>
+          //         <Card.Description>Price: {product.price} $</Card.Description>
+          //       </>
+          //     )}
+          //   </Card.Content>
+          //
+          //   <Card.Content extra>
+          //     <Link to={`/products/show/${product.id}`}>
+          //       <Button disabled={loading} >Show</Button>
+          //     </Link>
+          //     <Link to={`/products/${product.id}`}>
+          //       <Button disabled={loading} primary>Edit</Button>
+          //     </Link>
+          //     {deleteCheck===product.id?
+          //       <Button loading>Loading</Button>:
+          //       <Button disabled={loading} onClick={()=>deleteProduct(product.id)}>Delete</Button>}
+          //   </Card.Content>
+          // </Card>
         ))}
       </Card.Group>
     </>
